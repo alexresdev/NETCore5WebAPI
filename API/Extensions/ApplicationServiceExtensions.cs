d539e7e5fbd04976a85d499b3b8e117fd8373760
alexresdev <alexresdev@gmail.com>
2021-08-06 13:51:37 +0300

    API Image upload

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -9,6 +9,8 @@ using Application.Core;
 using Application.Activities;
 using Application.Interfaces;
 using Infrastructure.Security;
+using Infrastructure.Photos;
+using Application.Photos;
 
 namespace API.Extensions
 {
@@ -35,6 +37,8 @@ namespace API.Extensions
             services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
             services.AddScoped<IUserAccessor, UserAccessor>();
+            services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+            services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
 
             return services;
         }
